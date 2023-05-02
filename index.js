95b52fa8d541aaa2364ae3f5f3f8f809f787d1d8
Daniel Ruiz <danielruimar@gmail.com>
2023-05-02 20:19:48 +0200

    add await to connect

--- index.js
+++ index.js
@@ -4,32 +4,38 @@ const express = require("express");
 // Importamos las userRoutes
 const { userRouter } = require("./routes/user.routes"); //  LO IMPORTAMOS COMO UN OBJETO
 const { carRouter } = require("./routes/car.routes"); //  LO IMPORTAMOS COMO UN OBJETO
-// Conexión a la base de datos
-const { connect } = require("./db.js"); // Importamos el archivo de conexión a la BBDD
-connect(); //  Conectamos con la BBDD
 
-//  Configuración del server
-const PORT = 3000; //  Definimos el puerto
-const server = express();
-server.use(express.json()); // Sepa interpretar los JSON
-server.use(express.urlencoded({ extended: false })); //  Sepa interpretar bien los parametros de las rutas
+const main = async () => {
+  // Conexión a la base de datos
+  const { connect } = require("./db.js"); // Importamos el archivo de conexión a la BBDD
+  await connect(); //  Conectamos con la BBDD
 
-//  Rutas
-const router = express.Router();
-router.get("/", (req, res) => {
-  res.send("Esta es la Home de nuestra API");
-});
-// Para que todas las peticiones que no se correspondan con nuestras rutas den un codigo 404 y manden un mensaje de error.
-router.get("*", (req, res) => {
-  res.status(404).send("Lo sentimos :( No hemos encontrado la página requerida.");
-});
+  //  Configuración del server
+  const PORT = 3000; //  Definimos el puerto
+  const server = express();
+  server.use(express.json()); // Sepa interpretar los JSON
+  server.use(express.urlencoded({ extended: false })); //  Sepa interpretar bien los parametros de las rutas
 
-//  Usamos las rutas (el orden es importante más restrictivos a menos)
-server.use("/car", carRouter); //  Le decimos al server que utilice el carRouter importado para gestionar las rutas que tengan "/car"
-server.use("/user", userRouter); //  Le decimos al server que utilice el userRouter importado para gestionar las rutas que tengan "/user"
-server.use("/", router); //  Decimos al server que utilice el router en la raíz
+  //  Rutas
+  const router = express.Router();
+  router.get("/", (req, res) => {
+    res.send("Esta es la Home de nuestra API");
+  });
 
-//  Levantamos el server en el puerto indicado
-server.listen(PORT, () => {
-  console.log(`Server levantado en puerto ${PORT}`);
-});
+  // Para que todas las peticiones que no se correspondan con nuestras rutas den un codigo 404 y manden un mensaje de error.
+  router.get("*", (req, res) => {
+    res.status(404).send("Lo sentimos :( No hemos encontrado la página requerida.");
+  });
+
+  //  Usamos las rutas (el orden es importante más restrictivos a menos)
+  server.use("/car", carRouter); //  Le decimos al server que utilice el carRouter importado para gestionar las rutas que tengan "/car"
+  server.use("/user", userRouter); //  Le decimos al server que utilice el userRouter importado para gestionar las rutas que tengan "/user"
+  server.use("/", router); //  Decimos al server que utilice el router en la raíz
+
+  //  Levantamos el server en el puerto indicado
+  server.listen(PORT, () => {
+    console.log(`Server levantado en puerto ${PORT}`);
+  });
+};
+
+main();
