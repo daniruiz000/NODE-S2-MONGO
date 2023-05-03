84e61c2e245436d4efc1ec8513518e5ef448c318
Daniel Ruiz <danielruimar@gmail.com>
2023-05-03 07:54:24 +0200

    fix problems

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-// Importamos express
+// Importamos express.
 const express = require("express");
 
-// Importamos las userRoutes
-const { userRouter } = require("./routes/user.routes"); //  LO IMPORTAMOS COMO UN OBJETO
-const { carRouter } = require("./routes/car.routes"); //  LO IMPORTAMOS COMO UN OBJETO
+// Importamos las userRoutes:
+const { userRouter } = require("./routes/user.routes"); //  LO IMPORTAMOS COMO UN OBJETO.
+const { carRouter } = require("./routes/car.routes"); //  LO IMPORTAMOS COMO UN OBJETO.
 
 const main = async () => {
-  // Conexión a la base de datos
-  const { connect } = require("./db.js"); // Importamos el archivo de conexión a la BBDD
-  const database = await connect(); //  Conectamos con la BBDD
+  // Conexión a la base de datos.
+  const { connect } = require("./db.js"); // Importamos el archivo de conexión a la BBDD.
+  const database = await connect(); //  Conectamos con la BBDD.
 
-  //  Configuración del server
-  const PORT = 3000; //  Definimos el puerto
+  //  Configuración del server:
+  const PORT = 3000; //  Definimos el puerto.
   const server = express();
-  server.use(express.json()); // Sepa interpretar los JSON
-  server.use(express.urlencoded({ extended: false })); //  Sepa interpretar bien los parametros de las rutas
+  server.use(express.json()); // Sepa interpretar los JSON.
+  server.use(express.urlencoded({ extended: false })); //  Sepa interpretar bien los parametros de las rutas.
 
-  //  Rutas
-  const router = express.Router();
+  //  Rutas:
+  const router = express.Router(); // Definimos el router que será el encargado de manejar las peticiones a nuestras rutas.
+
   router.get("/", (req, res) => {
     res.send(`Esta es la Home de nuestra API. Estamos usando la BBDD de ${database.connection.name}`);
   });
@@ -27,15 +28,15 @@ const main = async () => {
     res.status(404).send("Lo sentimos :( No hemos encontrado la página requerida.");
   });
 
-  //  Usamos las rutas (el orden es importante más restrictivos a menos)
-  server.use("/car", carRouter); //  Le decimos al server que utilice el carRouter importado para gestionar las rutas que tengan "/car"
-  server.use("/user", userRouter); //  Le decimos al server que utilice el userRouter importado para gestionar las rutas que tengan "/user"
-  server.use("/", router); //  Decimos al server que utilice el router en la raíz
+  //  Usamos las rutas (el orden es importante más restrictivos a menos):
+  server.use("/car", carRouter); //  Le decimos al server que utilice el carRouter importado para gestionar las rutas que tengan "/car".
+  server.use("/user", userRouter); //  Le decimos al server que utilice el userRouter importado para gestionar las rutas que tengan "/user".
+  server.use("/", router); //  Decimos al server que utilice el router en la raíz.
 
-  //  Levantamos el server en el puerto indicado
+  //  Levantamos el server en el puerto indicado:
   server.listen(PORT, () => {
     console.log(`Server levantado en puerto ${PORT}`);
   });
 };
 
-main();
+main(); //  Llamamos a la función.
