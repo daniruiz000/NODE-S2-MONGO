3fd9746aaf5029a7da379a3f01b608db1370a586
Daniel Ruiz <danielruimar@gmail.com>
2023-05-09 09:22:49 +0200

    fix set

--- front/src/App.js
+++ front/src/App.js
@@ -5,15 +5,17 @@ function App() {
   const API_URL = "https://node-s2-mongo.vercel.app/";
   const [brands, setBrands] = React.useState();
   React.useEffect(() => {
-    fetch(API_URL).then((brands) => setBrands(brands));
+    fetch(API_URL)
+      .then((brands) => brands.json())
+      .then((brandsParsed) => setBrands(brandsParsed));
   }, []);
 
   return (
     <div className="App">
       <h2>Marcas</h2>
       <ul>
-        {brands?.map((brand) => {
-          return <li>{brand.name}</li>;
+        {brands?.data.map((brand) => {
+          return <li key={brand._id}>{brand.name}</li>;
         })}
       </ul>
     </div>
