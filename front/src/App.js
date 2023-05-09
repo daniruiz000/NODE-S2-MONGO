0c9e43c9a867e99472b7156bb8c1231f3daa4691
Daniel Ruiz <danielruimar@gmail.com>
2023-05-09 08:47:31 +0200

    add front and cors

---
+++ front/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import React from "react";
+function App() {
+  const API_URL = "https://node-s2-mongo.vercel.app/";
+  const [brands, setBrands] = React.useState();
+  React.useEffect(() => {
+    fetch(API_URL).then((brands) => setBrands(brands));
+  }, []);
+  return (
+    <div className="App">
+      <h2>Marcas</h2>
+      <ul>
+        {brands?.map((brand) => {
+          return <li>{brand.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
